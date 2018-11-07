d97daf30c5da341c7f66168d6153bdc7a5451f74
Josh Preuss <jopreuss@amazon.com>
2018-11-07 11:00:43 -0500

    Changing tests

--- test_sameness.py
+++ test_sameness.py
@@ -1,4 +1,6 @@
 import io
+import subprocess
 with io.open('tst-cmds', 'r') as f:
     test_cmds = [line.strip() for line in f]
-print(test_cmds)
+for l in test_cmds:
+    subprocess.check_call("cat ~/hii | {}".format(l), shell=True)
